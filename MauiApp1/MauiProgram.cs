607ffd8e688eeca74d0d2483bed8e8722671329e
DimitriLit <d.litvinenko1905@gmail.com>
2025-11-25 15:35:50 +0200

    second commit

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("Montserrat-Medium.ttf", "RegularFont");
+                fonts.AddFont("Montserrat-SemiBold.ttf", "MediumFont");
+                fonts.AddFont("Montserrat-Bold.ttf", "BoldFont");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
