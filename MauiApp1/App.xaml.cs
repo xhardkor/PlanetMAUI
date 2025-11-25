6ac3500f26f854dae7e730bb47ba2799ff0231b3
DimitriLit <d.litvinenko1905@gmail.com>
2025-11-25 15:51:10 +0200

    second commit

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -1,14 +1,39 @@
-﻿namespace MauiApp1;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+
+
+namespace MauiApp1;
 
 public partial class App : Application
 {
+    const int WindowWidth = 540;
+    const int WindowHeight = 768;
+
     public App()
     {
         InitializeComponent();
+
+        #if WINDOWS
+        Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+        {
+            var mauiWindow = handler.VirtualView;
+            var nativeWindow = handler.PlatformView;
+            nativeWindow.Activate();
+            IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+            WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+            AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+            appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+        });
+        #endif
+
+        //MainPage = new CalculatorPage();
     }
 
-    protected override Window CreateWindow(IActivationState? activationState)
-    {
-        return new Window(new AppShell());
-    }
+    //protected override Window CreateWindow(IActivationState? activationState)
+    //{
+    //    return new Window(new AppShell());
+    //}
 }
